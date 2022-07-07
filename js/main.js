bf3aefbb6c44e12d3e5eb2a4e0c577ca707c03da
daihoidoanou <daihoidoanou@gmail.com>
2022-07-08 00:28:59 +0700

    first launch

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Set the date we're counting down to
+var countDownDate = new Date("July 23, 2022 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.querySelector("#countdown .days div").innerText = days;
+  document.querySelector("#countdown .hours div").innerText = hours;
+  document.querySelector("#countdown .minutes div").innerText = minutes;
+  document.querySelector("#countdown .seconds div").innerText = seconds;
+  //document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  //+ minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
