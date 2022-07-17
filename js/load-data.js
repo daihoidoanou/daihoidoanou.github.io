cd31b5d1533361c032a999484479a012178cde1a
Nguyen Truong Vu <69038670+truongvu151@users.noreply.github.com>
2022-07-17 22:37:29 +0700

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -1,36 +1,32 @@
-(async () => {
-    await axios
-        .get("https://dai-hoi-xi.herokuapp.com/post/all-post")
-        .then((res) => {
-            console.log(res.data)
-            htmls = res.data?.posts
-                .map((item) => {
-                    return `
-                        <div class="main-right__item">
-                            <div class="main-right__item-avt">
-                                <img src=${item?.avt} alt="" width="100px" />
-                            </div>
-                            <div class="main-right__item-body">
-                                <div class="main-right__item-text">
-                                    ${item?.content}<span>, ${m}</span> phút trước đã nói </span>
+ (async () => {
+                await axios
+                    .get("https://dai-hoi-xi.herokuapp.com/post/all-post")
+                    .then((res) => {
+                        const htmls = res.data?.posts
+                            .map((item) => {
+                                return `
+                            <div class="main-right__item">
+                                <div class="main-right__item-avt">
+                                    <img src=${item?.avt} alt="" />
                                 </div>
-                                <div class="main-right__item-name">
-                                    ${item?.name}
+                                <div class="main-right__item-body">
+                                    <div class="main-right__item-name">
+                                        ${item?.name}
+                                    </div>
+                                    <div class="main-right__item-text">
+                                        ${item?.content}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        `;
-                })
-                .join("");
+                            `;
+                            })
+                            .join("");
+                        $(".main-right").innerHTML = htmls;
+                        $(".main-heading").querySelector("span").innerHTML =
+                            res.data?.total;
+                    })
+                    .catch((err) => console.log(err.response?.data));
+            })();
 
-            $(".main-right").innerHTML = htmls;
-            $(".main-heading").querySelector("span").innerHTML =
-                res.data?.total;
-        })
-        .catch((err) => console.log(err.response?.data));
-})();
-
-
-$(".main-submit").onclick = () =>
-
-    (window.location.href = "./send-wishes.html");
+            $(".main-submit").onclick = () =>
+                (window.location.href = "./index.html");
