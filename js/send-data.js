a8b4c64298d0f9fdb8a9dfe5df3fc22d7431fb90
Nguyen Truong Vu <69038670+truongvu151@users.noreply.github.com>
2022-07-17 23:35:10 +0700

    Update send-data.js

--- js/send-data.js
+++ js/send-data.js
@@ -1,124 +1,124 @@
-const app = (() => {
-    const avtLS =
-        JSON.parse(localStorage.getItem("avtLS")) ||
-        "https://cdn4.iconfinder.com/data/icons/user-gradient-set-1/128/user_action_set_2-22-512.png";
+ const app = (() => {
+                const avtLS =
+                    JSON.parse(localStorage.getItem("avtLS")) ||
+                    "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png";
 
-    return {
-        isLoading: false,
-        handleEvent() {
-            const _this = this;
-            const inputs = $$(".main-right__form-input");
-            const mainLeftWrap = $(".main-left__wrap");
-            const loading = $(".main-left__wrap .loading");
+                return {
+                    isLoading: false,
+                    handleEvent() {
+                        const _this = this;
+                        const inputs = $$(".main-right__form-input");
+                        const mainLeftWrap = $(".main-left__wrap");
+                        const loading = $(".main-left__wrap .loading");
 
-            mainLeftWrap.querySelector("img").src = avtLS;
+                        mainLeftWrap.querySelector("img").src = avtLS;
 
-            inputs.forEach((input) => {
-                input.oninput = function (e) {
-                    const span =
-                        this.parentElement.querySelector(
-                            ".count-char__now"
-                        );
-                    const max = parseInt(
-                        this.parentElement.querySelector(
-                            ".count-char__max"
-                        ).innerHTML
-                    );
-
-                    const lengthInp = input.value.length;
-
-                    if (lengthInp <= max) {
-                        span.innerHTML = lengthInp;
-                    } else {
-                        this.blur();
-                    }
-                };
-            });
-
-            mainLeftWrap.onclick = function () {
-                if (!_this.isLoading) {
-                    this.parentElement
-                        .querySelector(".main-left__inp")
-                        .click();
-                }
-            };
-
-            // loading
-            startLoading = () => {
-                _this.isLoading = true;
-                loading.classList.add("active");
-            };
-
-            endLoading = () => {
-                _this.isLoading = false;
-                loading.classList.remove("active");
-            };
-
-            $(".main-left__inp").onchange = (e) => {
-                startLoading();
-                const file = e.target.files[0];
-                if (file) {
-                    const reader = new FileReader();
-                    reader.readAsDataURL(file);
-                    reader.onloadend = async () => {
-                        await axios
-                            .post(
-                                "https://dai-hoi-xi.herokuapp.com/post/upload-avt",
-                                {
-                                    data: reader.result,
-                                }
-                            )
-                            .then((res) => {
-                                localStorage.setItem(
-                                    "avtLS",
-                                    JSON.stringify(res.data)
+                        inputs.forEach((input) => {
+                            input.oninput = function (e) {
+                                const span =
+                                    this.parentElement.querySelector(
+                                        ".count-char__now"
+                                    );
+                                const max = parseInt(
+                                    this.parentElement.querySelector(
+                                        ".count-char__max"
+                                    ).innerHTML
                                 );
-                                mainLeftWrap.querySelector(
-                                    "img"
-                                ).src = res.data;
-                            })
-                            .catch((err) => {
-                                console.log(err.response?.data);
-                            })
-                            .finally(() => {
-                                setTimeout(endLoading, 2000);
-                            });
-                    };
-                }
-            };
 
-            $(".main-right__form-submit-btn").onclick = async (
-                e
-            ) => {
-                e.preventDefault();
-                const avt = mainLeftWrap.querySelector("img").src;
-                const content = inputs[0].value;
-                const name = inputs[1].value;
-                if (avt && name && content) {
-                    const data = {
-                        avt,
-                        name,
-                        content,
-                    };
+                                const lengthInp = input.value.length;
 
-                    await axios
-                        .post(
-                            "https://dai-hoi-xi.herokuapp.com/post/new-post",
-                            data
-                        )
-                        .then((res) => {
-                            inputs[0].value = null;
-                            inputs[1].value = null;
-                            window.location.href = "./index.html";
-                        })
-                        .catch((err) =>
-                            console.log(err.response?.data)
-                        );
-                }
-            };
-        },
-        start() {
-            this.handleEvent();
-        },
-    };
-})().start();
+                                if (lengthInp <= max) {
+                                    span.innerHTML = lengthInp;
+                                } else {
+                                    this.blur();
+                                }
+                            };
+                        });
+
+                        mainLeftWrap.onclick = function () {
+                            if (!_this.isLoading) {
+                                this.parentElement
+                                    .querySelector(".main-left__inp")
+                                    .click();
+                            }
+                        };
+
+                        // loading
+                        startLoading = () => {
+                            _this.isLoading = true;
+                            loading.classList.add("active");
+                        };
+
+                        endLoading = () => {
+                            _this.isLoading = false;
+                            loading.classList.remove("active");
+                        };
+
+                        $(".main-left__inp").onchange = (e) => {
+                            startLoading();
+                            const file = e.target.files[0];
+                            if (file) {
+                                const reader = new FileReader();
+                                reader.readAsDataURL(file);
+                                reader.onloadend = async () => {
+                                    await axios
+                                        .post(
+                                            "https://dai-hoi-xi.herokuapp.com/post/upload-avt",
+                                            {
+                                                data: reader.result,
+                                            }
+                                        )
+                                        .then((res) => {
+                                            localStorage.setItem(
+                                                "avtLS",
+                                                JSON.stringify(res.data)
+                                            );
+                                            mainLeftWrap.querySelector(
+                                                "img"
+                                            ).src = res.data;
+                                        })
+                                        .catch((err) => {
+                                            console.log(err.response?.data);
+                                        })
+                                        .finally(() => {
+                                            setTimeout(endLoading, 2000);
+                                        });
+                                };
+                            }
+                        };
+
+                        $(".main-right__form-submit-btn").onclick = async (
+                            e
+                        ) => {
+                            e.preventDefault();
+                            const avt = mainLeftWrap.querySelector("img").src;
+                            const content = inputs[0].value;
+                            const name = inputs[1].value;
+                            if (avt && name && content) {
+                                const data = {
+                                    avt,
+                                    name,
+                                    content,
+                                };
+
+                                await axios
+                                    .post(
+                                        "https://dai-hoi-xi.herokuapp.com/post/new-post",
+                                        data
+                                    )
+                                    .then((res) => {
+                                        inputs[0].value = null;
+                                        inputs[1].value = null;
+                                        window.location.href = "./home.html";
+                                    })
+                                    .catch((err) =>
+                                        console.log(err.response?.data)
+                                    );
+                            }
+                        };
+                    },
+                    start() {
+                        this.handleEvent();
+                    },
+                };
+            })().start();
